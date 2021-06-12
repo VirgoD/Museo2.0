f87e1ad4e3a5f81d966d1e36bc642610ee851726
virginia <virginia.d@deas.it>
2021-06-12 21:59:00 +0200

    fix login

--- src/main/java/it/uniroma3/siw/rentalev/model/User.java
+++ src/main/java/it/uniroma3/siw/rentalev/model/User.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
-@Table(	name = "users",
+@Table(	name = "users", schema = "utente",
 		uniqueConstraints = { 
 			@UniqueConstraint(columnNames = "username"),
 			@UniqueConstraint(columnNames = "email") 
@@ -46,7 +46,7 @@ public class User {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(	name = "user_roles", 
+	@JoinTable(	name = "user_roles", schema = "utente",
 				joinColumns = @JoinColumn(name = "user_id"), 
 				inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
@@ -58,5 +58,44 @@ public class User {
 		this.username = username;
 		this.email = email;
 		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
 }
