186c3fed970ea307923b0e1ac03e470bb48238eb
virginia <virginia.d@deas.it>
2021-06-16 19:03:37 +0200

    t

--- front_end/src/services/artist.service.js
+++ front_end/src/services/artist.service.js
@@ -7,7 +7,7 @@ const API_URL = 'http://localhost:8080/api/v1/';
 class ArtistService {
 
     getAllArtists() {
-        return axios.get(API_URL + 'artisti', {headers: authHeader() })}
+        return axios.get(API_URL + 'artisti' , {headers: authHeader() })}
 
     getAdminBoard() {
         return axios.get(API_URL + 'admin', { headers: authHeader() });
