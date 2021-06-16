229c90f78d8f8d246e5f761a5e43c847ee26761a
virginia <virginia.d@deas.it>
2021-06-16 19:41:18 +0200

    collection

--- front_end/src/components/Artist.vue
+++ front_end/src/components/Artist.vue
@@ -47,7 +47,6 @@ export default {
       if (this.currentUser && this.currentUser.roles) {
         return this.currentUser.roles.includes('ROLE_ADMIN');
       }
-
       return false;
     },
   },
--- front_end/src/components/Collection.vue
+++ front_end/src/components/Collection.vue
@@ -2,6 +2,11 @@
   <div class="container w-75 bg-light mt-5 p-4">
     <h2 class="pt-3" style="text-align:center">All Collections</h2>
     <div class="" style="text-align:right">
+      <div v-if="showAdminBoard" class="navbar-nav ml-auto">
+        <form id="collection-search" v-on:submit.prevent>
+          <input type="text" placeholder="Ricerca collezione" name="collezioneName" />
+          <button type="submit" class="btn btn-primary btn-sm mt-3" @click="searchCollezione">Ricerca</button>
+        </form>
       <router-link class="btn btn-outline-success btn-sm" to="/addCollection">Inserisci Collezione
       </router-link>
       <router-link class="btn btn-outline-success btn-sm" to="/modificaCollection">Modifica Collezione
@@ -22,13 +27,28 @@
       </div>
     </div>
   </div>
+  </div>
 </template>
 
 <script>
 import CollectionService from '../services/collection.service';
+import axios from "axios";
+import authHeader from "@/services/auth-header";
 
 
 export default {
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    showAdminBoard() {
+      if (this.currentUser && this.currentUser.roles) {
+        return this.currentUser.roles.includes('ROLE_ADMIN');
+      }
+
+      return false;
+    },
+  },
   data() {
     return {
       collections: [],
@@ -50,6 +70,20 @@ export default {
   },
 
   methods : {
+    searchCollezione() {
+      const form = document.getElementById('collection-search');
+      const formData = new FormData(form);
+      axios
+          .get("http://localhost:8080/api/v1/collezioneSpecifico", {
+            params: {
+              titolo: formData.get('collezioneName')
+            }, headers: authHeader()
+          })
+          .then((response) => {
+            this.collections = response.data;
+          });
+    },
+
     deleteCollection(id, event) {
       console.log(id);
       axios.delete("http://localhost:8080/api/v1/delete" + id).then((response) => {
@@ -61,6 +95,7 @@ export default {
         event.target.parentElement.parentElement.remove();
       }, 1000);
     },
+
     updateCollection(id) {
       this.$router.push({ path: "/collectionUpdate/" + id });
     },
