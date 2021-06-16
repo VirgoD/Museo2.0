6973728b280439458409cf11162c9a7c470e9882
virginia <virginia.d@deas.it>
2021-06-16 17:49:03 +0200

    collection

--- front_end/src/components/Collection.vue
+++ front_end/src/components/Collection.vue
@@ -2,6 +2,11 @@
   <div class="container w-75 bg-light mt-5 p-4">
     <h2 class="pt-3" style="text-align:center">All Collections</h2>
     <div class="" style="text-align:right">
+      <form id="collection-search" v-on:submit.prevent>
+        <input type="text" placeholder="Ricerca collezione" name="titolo"/>
+        <button type="submit" class="btn btn-primary btn-sm mt-3" @click="searchCollection">Ricerca</button>
+      </form>
+
       <router-link class="btn btn-outline-success btn-sm" to="/addCollection">Inserisci Collezione
       </router-link>
       <router-link class="btn btn-outline-success btn-sm" to="/modificaCollection">Modifica Collezione
@@ -26,6 +31,8 @@
 
 <script>
 import CollectionService from '../services/collection.service';
+import axios from "axios";
+import authHeader from "@/services/auth-header";
 
 
 export default {
@@ -50,6 +57,19 @@ export default {
   },
 
   methods : {
+    searchCollection() {
+      const form = document.getElementById('collection-search');
+      const formData = new FormData(form);
+      axios
+          .get("http://localhost:8080/api/v1/collezioneSpecifico", {
+            params: {
+              titolo: formData.get('titolo')
+            }, headers: authHeader()
+          })
+          .then((response) => {
+            this.collections = response.data;
+          });
+    },
     deleteCollection(id, event) {
       console.log(id);
       axios.delete("http://localhost:8080/api/v1/delete" + id).then((response) => {
